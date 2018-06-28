c91078b8e165f7c5d0331258a38c58f21c00a422
andy <308956720@qq.com>
2018-06-28 20:57:10 +0800

    添加ConfigurationOptions转换

---
+++ CSRedis.NetCore.Tests/ConfigurationOptionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSRedis.NetCore.Tests
+{
+    public class ConfigurationOptionsTest
+    {
+		[Fact]
+		public void Test()
+		{
+			var str = "192.168.1.166:7001,192.168.1.166:7002,defaultDatabase=1,serviceName=session-master,password=123456,ssl=false";
+			var config = ConfigurationOptions.Parse(str);
+		}
+    }
+}
